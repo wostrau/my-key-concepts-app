86d8675afd85cb84c15a6b27a55ca70516518461
wostrau <a.ostrovtsov@gmail.com>
2023-07-16 16:11:11 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import keyConceptsImage from './assets/images/key-concepts.png';
+import componentsImage from './assets/images/components.png';
+import stateImage from './assets/images/state.png';
+import eventsImage from './assets/images/events.png';
 
-function App() {
+type ConceptType = { title: string; image: string; description: string };
+const concepts: ConceptType[] = [
+  {
+    title: 'Components',
+    image: componentsImage,
+    description:
+      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+  },
+  {
+    title: 'State',
+    image: stateImage,
+    description:
+      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+  },
+  {
+    title: 'Events',
+    image: eventsImage,
+    description:
+      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={keyConceptsImage} alt='Medal badge with a star' />
+        <h1>Key React Concepts</h1>
+        <p>Selected key React concepts you should know about</p>
       </header>
+      <ul id='concepts'>
+        {concepts.map((concept) => {
+          return (
+            <li className='concept'>
+              <img src={concept.image} alt={concept.title} />
+              <h2>{concept.title}</h2>
+              <p>{concept.description}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
